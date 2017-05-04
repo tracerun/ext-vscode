5dbfc4ee63408ea93f2172dc904d5d07ad287e6d
drkaka <drkaka@drkaka.com>
2017-05-04 18:13:36 +0800

    Add methods.

--- extension.js
+++ extension.js
@@ -1,35 +1,132 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const window = vscode.window;
+const tracerun = require('tracerun-cli');
+const moment = require('moment');
+
+let ready = false;
+let preparing = false;
+let statusBar;
+let cli = new tracerun.client.Client();
+let chan = window.createOutputChannel("TraceRun");
+
+tracerun.client.setErrFunc(err => {
+  console.error(err);
+});
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-function activate(context) {
+exports.activate = function (context) {
+  statusBar = window.createStatusBarItem(vscode.StatusBarAlignment.Left);
+  statusBar.show();
 
-  console.log("file:", vscode.window.activeTextEditor.document.fileName);
+  addDocument(window.activeTextEditor.document);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.sayHello', function() {
-    // The code you place here will be executed every time your command is executed
+  // search for slots
+  context.subscriptions.push(getSearchSlotsCommand());
 
-    // Display a message box to the user
-    vscode.window.showInformationMessage('Hello World!');
+  // search for targets
+  context.subscriptions.push(getSearchTargetCommand());
+
+  window.onDidChangeActiveTextEditor(e => {
+    addDocument(e.document);
   });
 
-  vscode.window.onDidChangeActiveTextEditor(e => {
-    console.log(e.document.fileName);
+  window.onDidChangeTextEditorSelection(e => {
+    addDocument(e.textEditor.document);
   });
-
-  vscode.window.onDidChangeTextEditorSelection(e => {
-    console.log("changed");
-  });
-
-  context.subscriptions.push(disposable);
-}
-exports.activate = activate;
+};
 
 // this method is called when your extension is deactivated
-function deactivate() {}
-exports.deactivate = deactivate;
+exports.deactivate = function () {
+};
+
+function getSearchSlotsCommand() {
+  return vscode.commands.registerCommand('extension.tracerun.slots', function () {
+    let targetOptions = {
+      placeHolder: 'target to search',
+      value: "",
+      ignoreFocusOut: true
+    };
+    window.showInputBox(targetOptions).then(function (target) {
+      if (!target || target.length === 0) {
+        window.showWarningMessage("target incorrect.");
+      } else {
+        let rangeOptions = {
+          prompt: '"2000-01-02 03:04:56 to 2011-02-03 04:30:05"',
+          placeHolder: 'Please use this format',
+          value: "",
+          ignoreFocusOut: true
+        };
+        window.showInputBox(rangeOptions).then(function (val) {
+          let arr = val.split("to");
+          if (arr.length != 2) {
+            window.showWarningMessage("Date range incorrect.");
+          } else {
+            let start = moment(arr[0]);
+            if (!start.isValid()) {
+              window.showWarningMessage("Date range incorrect.");
+              return;
+            }
+
+            let end = moment(arr[1]);
+            if (!end.isValid()) {
+              window.showWarningMessage("Date range incorrect.");
+              return;
+            }
+
+            cli.getSlots(target, start.unix(), end.unix(), slots => {
+              chan.clear();
+              chan.appendLine("slots: " + "(target: " + target + ")");
+              let all = slots.slotsList;
+              for (var i = 0; i < all.length; i++) {
+                chan.appendLine(all[i].start + ": " + all[i].slot);
+              }
+              chan.show();
+            });
+          }
+        });
+      }
+    });
+  });
+}
+
+function getSearchTargetCommand() {
+  return vscode.commands.registerCommand('extension.tracerun.targets', function () {
+    chan.clear();
+    chan.appendLine("targets:");
+    cli.getTargets(list => {
+      let targets = list.targetList;
+      for (var i = 0; i < targets.length; i++) {
+        chan.appendLine((i + 1) + ": " + targets[i]);
+      }
+      chan.show();
+    });
+  });
+}
+
+function prepareTraceRun() {
+  statusBar.text = "$(clock) TraceRun preparing";
+  preparing = true;
+
+  tracerun.agent.start(err => {
+    if (err) {
+      console.error(err);
+    } else {
+      ready = true;
+      statusBar.text = "$(clock) TraceRun Active";
+    }
+    preparing = false;
+  });
+}
+
+function addDocument(doc) {
+  if (!ready && !preparing) {
+    prepareTraceRun();
+  } else if (ready && !preparing) {
+    if (doc.uri.scheme === "file") {
+      cli.addAction(doc.fileName);
+    }
+  }
+}
